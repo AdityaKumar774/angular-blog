57567f0d4c4201bba3778d6f49436bef66287fd5
Aditya Kumar <aditya.kumar774@gmail.com>
2017-09-02 12:49:13 +0530

    not working and i dont know why!

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@ angular.module('app', [
     'ngRoute',
     'app.controllers'
 ])
-    .config(['$routeProvider', function($routeProvider){
+    .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/',{
             templateUrl : 'views/post.html',
             controller : 'PostController'
-        }).when('/post/:id',{
+        }).when('/post/:id', {
             templateUrl: 'views/singlepost.html',
             controller: 'SinglePostController'
         }).otherwise({
--- app/controllers.js
+++ app/controllers.js
@@ -1,13 +1,13 @@
 angular.module('app.controllers', [
     'app.directives'
 ])
-    .controller('PostController', ['$scope', '$http', function ($scope, $http) {
+    .controller('PostController', ['$scope', '$http', function ($scope, $http){
         $http.get('data/posts.json').then(function (data) {
             $scope.posts = data.data;
         });
     }])
     .controller('SinglePostController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-        $http.get('data/posts.json').then(function(data){
+        $http.get('data/posts.json').then(function (data){
             $scope.post = data[$routeParams.id];
         });
     }]);
